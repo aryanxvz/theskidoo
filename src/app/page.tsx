255c2b6795ad9a710b83064817e2832866a1ae35
Aryan Mane <aryanmane7916@gmail.com>
2024-10-06 20:01:36 +0530

    added cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { Cards } from "@/components/Cards";
 import { Features } from "@/components/Features";
 import { HeroPage } from "@/components/HeroPage";
 import { Steps } from "@/components/Steps";
+import { Testimonials } from "@/components/Testimonials";
 
 export default function Home() {
     return (
@@ -10,6 +12,8 @@ export default function Home() {
                 <Steps />
                 <Features />
             </div>
+            <Cards />
+            <Testimonials />
         </div>
     )
 }
---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,10 @@
+
+export const Testimonials = () => {
+    return (
+        <section>
+            <div>
+                
+            </div>
+        </section>
+    )
+}
